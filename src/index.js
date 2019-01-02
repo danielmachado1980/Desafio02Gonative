b39db23b15be88fd104c23fb0e4b0d76f82c9bda
Daniel Machado <daniel.g_amachado@hotmail.com>
2019-01-02 08:19:51 -0200

    Boilerplate RN

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Bem-Vindo ao React Native!
+          Bem-Vindo ao Boilerplate RN !
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
